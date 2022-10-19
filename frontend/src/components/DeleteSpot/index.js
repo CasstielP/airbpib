b2950c77bd25dd38ab1b40f70bb147fa926252ec
Your Name <Your Email>
2022-10-19 14:16:55 -0700

    up to get all user review

---
+++ frontend/src/components/DeleteSpot/index.js
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from "react-redux";
+import {deleteSpotThunk} from '../../store/spot'
+import { useHistory, useParams } from "react-router-dom";
+
+
+const DeleteSpotForm = () => {
+    const {spotId} = useParams()
+    const history = useHistory()
+    const dispatch = useDispatch()
+    const spot = useSelector(state=> state.spot.singleSpot)
+
+    if(!spot) {
+        alert('Spot not found!')
+    }
+
+    const handleDeleteSpot =()=> {
+        const spotIdNum = Number(spotId)
+        dispatch(deleteSpotThunk(spotIdNum))
+        history.push('/')
+    }
+
+    return (
+        <div>
+            <button onClick={handleDeleteSpot}>Delete Spot</button>
+        </div>
+    )
+}
+
+export default DeleteSpotForm;
---
+++ frontend/src/components/Review/index.js
@@ -0,0 +1,3 @@
+// import { useDispatch, useSelector } from "react-redux";
+
+// const reviewPortion = ({review})
